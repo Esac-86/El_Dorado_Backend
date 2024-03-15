dec83c0c33f8575ed02473c86f6f98fa17cc9234
Esac-86 <162314344+Esac-86@users.noreply.github.com>
2024-03-15 06:33:43 -0500

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'bv1arrxtrcp2ih1y1ios-mysql.services.clever-cloud.com',
-  user: 'ucespbzk59n6rncp',
-  password: '3vwZkHNDr8jr3fFTJwpt',
-  database: 'bv1arrxtrcp2ih1y1ios',
+  host: 'monorail.proxy.rlwy.net',
+  user: 'root',
+  password: 'XfrfoAMxQGiCYmVGNKBUKsaUZzuBUSrk',
+  database: 'railway',
+  port: 23707, 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
