7e6ce67599e206f70f779b3b220c44a82306c0f5
Esac-86 <162314344+Esac-86@users.noreply.github.com>
2024-03-14 19:57:22 -0500

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'id21976331_root',
-  password: 'Soydel2005!!XD',
-  database: 'id21976331_el_dorado_db',
+  host: 'bv1arrxtrcp2ih1y1ios-mysql.services.clever-cloud.com',
+  user: 'ucespbzk59n6rncp',
+  password: '3vwZkHNDr8jr3fFTJwpt',
+  database: 'bv1arrxtrcp2ih1y1ios',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
